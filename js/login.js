f51a8f89ff23b94ab3ba4aa8ad15d10937924bf6
Salem Yaslem <s@sy.sa>
2022-09-28 06:29:34 +0300

    add reCAPTCHA v3 and change url to go.lxpia.com

--- js/login.js
+++ js/login.js
@@ -3,35 +3,36 @@ const alert = document.getElementById("alert");
 
 loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  const email = event.target[0].value;
-  const password = event.target[1].value;
-
-  try {
-    const response = await fetch(
-      // 'https://api.lxpia.com/api/users/authenticate',
-      // 'https://api.dev.masterly.online/api/users/authenticate',
-      "https://api.new.lxpia.com/api/users/authenticate",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-        body: JSON.stringify({ email, password }),
-      }
-    );
-
-    const data = await response.json();
-
-    if (!data.token) {
-      alert.innerText = `Error: ${data.message || "unknown"}`;
-      alert.style.display = "block";
-    } else {
-      location.replace(`https://new.lxpia.com/auth/me?t=${data.token}`);
-    }
-  } catch (error) {
-    alert.innerText = `Error: ${error.message || "unknown"}`;
-    alert.style.display = "block";
-  }
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LdkhKUfAAAAADJBuUTN8si--Xv9R77_PZEZAA3t', {action: 'submit'}).then(async function(token) {
+        const email = event.target[0].value;
+        const password = event.target[1].value;
+      
+        try {
+          const response = await fetch(
+            "https://go.lxpia.com/api/users/authenticate",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+              },
+              body: JSON.stringify({ email, password, token, recaptcha: true, text: "" }),
+            }
+          );
+      
+          const data = await response.json();
+      
+          if (!data.token) {
+            alert.innerText = `Error: ${data.message || "unknown"}`;
+            alert.style.display = "block";
+          } else {
+            location.replace(`https://go.lxpia.com/auth/me?t=${data.token}`);
+          }
+        } catch (error) {
+          alert.innerText = `Error: ${error.message || "unknown"}`;
+          alert.style.display = "block";
+        }
+    });
+  });
 });
